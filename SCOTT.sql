3f97456cb386064fd151ea6fe93308a4823480d2
kdm9244 <86937594+kdm9244@users.noreply.github.com>
2026-02-02 11:13:35 +0900

    Update SCOTT.sql

--- SCOTT.sql
+++ SCOTT.sql
@@ -1,15 +1,16 @@
--- Ocacle (DBMS) - version(21C) -xe(database��)
--- user(scott) - ���̺�.
+-- Ocacle (DBMS) - version(21C) -xe(database명)
+-- user(scott) - 테이블.
 -- Structured Query Language (SQL)
---SELECT studno, name -- Į����(��ü��)
---FROM student; --���̺�.
+--SELECT studno, name -- 칼럼명(전체명)
+--FROM student; --테이블.
 
--- 1. professor  ������. ��ü �÷� ��ȸ.
+-- 1. professor  테이플. 전체 컬럼 조회.
 SELECT
     * FROM professor;
 
 
--- 2)�л� -> �л���ȣ, �̸�, �г�
+-- 2)학생 -> 학생번호, 이름, 학년
 SELECT PROFNO, name , grade
  FROM student;
     
+-- 숙제 완료함
