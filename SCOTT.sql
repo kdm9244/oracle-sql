7bfd95684016a60a0ffb9aef3c31b44d48a55094
kimdeokmo <sewonn201@naver.com>
2026-02-02 10:22:01 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,11 @@
+-- Ocacle (DBMS) - version(21C) -xe(database명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+--SELECT studno, name -- 칼럼명(전체명)
+--FROM student; --테이블.
+
+-- 1. professor  테이플. 전체 컬럼 조회.
+SELECT
+    * FROM professor;
+    
+-- 2.
